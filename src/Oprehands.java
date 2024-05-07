15ce6b289ed10cf4674f7d1eb00dbbb8830dad61
jaminel <jamineltechnology@gmail.com>
2024-05-07 21:01:33 +0100

    new update

--- src/Oprehands.java
+++ src/Oprehands.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Oprehands{
 
+    public static void basicOprehands(){
+        Scanner sc = new Scanner(System.in);
+        double meal_cost =sc.nextInt();
+        int tip_percent =sc.nextInt();
+        int tax_percent =sc.nextInt();
+        double addedTip=meal_cost/100*tip_percent;
+        double addedTax= tax_percent/100*meal_cost;
+        Double total = meal_cost+tip_percent+tax_percent;
+        System.out.println(total);
+    }
+
     public static void main(String[] args) {
+basicOprehands();
+
 
     }
 }
