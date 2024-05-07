a083eac68b4d69895b3f40041a21d58f99163e8b
jaminel <jamineltechnology@gmail.com>
2024-05-07 21:36:28 +0100

    update

--- src/Oprehands.java
+++ src/Oprehands.java
@@ -2,19 +2,27 @@ import java.util.Scanner;
 
 public class Oprehands{
 
-    public static void basicOprehands(){
-        Scanner sc = new Scanner(System.in);
-        double meal_cost =sc.nextInt();
-        int tip_percent =sc.nextInt();
-        int tax_percent =sc.nextInt();
-        double addedTip=meal_cost/100*tip_percent;
-        double addedTax= tax_percent/100*meal_cost;
-        Double total = meal_cost+tip_percent+tax_percent;
-        System.out.println(total);
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+
+        // Calculate added tax and tip using precise double values
+        double addedTax = (tax_percent / 100.0) * meal_cost;
+        double addedTip = (tip_percent / 100.0) * meal_cost;
+
+        // Calculate total cost without rounding
+        double totalCost = meal_cost + addedTax + addedTip;
+
+        // Round to nearest integer using Math.round()
+        long roundedTotal = Math.round(totalCost);
+
+        System.out.println(roundedTotal);
     }
 
     public static void main(String[] args) {
-basicOprehands();
+        Scanner sc = new Scanner(System.in);
+        double mealPrice= sc.nextDouble();
+        int tipPercent=sc.nextInt();
+        int taxPercent= sc.nextInt();
+solve(mealPrice,tipPercent,taxPercent);
 
 
     }
